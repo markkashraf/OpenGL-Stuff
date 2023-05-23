8ad14950bc5d54cf95d1797bf8db7fe6c4880d2c
markkashraf <mark2001rko2001@gmail.com>
2023-05-23 16:28:02 +0200

    Add project files.

---
+++ OpenGL/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "lol", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "failed" << "\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, 800, 800);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
